7eca346e970a449a5ac0385b00722577eabec5ff
nhan-bread <191059715+nhan-bread@users.noreply.github.com>
2025-08-19 14:23:38 -0700

    Add container with dynamically added grid squares Fits the grid squares into the container; isn't formatted into 16x16

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('#container');
+
+let square = 16;
+let squared = square * square;
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (i = 0; i < squared; i++) {
+        const grid = document.createElement('div');
+        container.appendChild(grid);
+        grid.setAttribute('style', 'outline: 1px solid black; height: 20px; width: 20px;');
+    }
+})
